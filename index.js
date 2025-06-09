fdb7b4f978808355f173a019d2db3a1c17f7c3e3
Lucasmcam <lucasmcam@gmail.com>
2025-06-09 10:11:18 -0300

    POST - PUT - DELETE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import {v4} from 'uuid'
 
 const port = 3000;
 
@@ -6,13 +7,72 @@ const tasks = [];
 
 const server = http.createServer((request, response) => {
 
-    const {method, url} = request;
+     const {method, url} = request;
+
+    let body = '';
+
+    request.on('data', (chunk) => {
+        body += chunk.toString();
+    })
+
+    request.on('end', () => {
+
+    const id = url.split('/')[2];
 
     if (url === '/tasks' && method === 'GET') {
 
-        response.writeHead(200, {"Content-Type": 'application.json'});
+        response.writeHead(200, {"Content-Type": 'application/json'});
         response.end(JSON.stringify(tasks));
+
+    } else if (url === '/tasks' && method === 'POST') {
+
+        const {title, description, status} = JSON.parse(body);
+        const newTask = {id: v4(), title, description, status}
+        tasks.push(newTask);
+        response.writeHead(201, {"Content-Type": 'application/json'});
+        response.end(JSON.stringify(newTask));
+    
+    } else if (url.startsWith ('/tasks') && method === 'PUT') {
+
+        const {title, description, status} = JSON.parse(body);
+        const taskToUpdate = tasks.find(task => task.id === id)
+
+        if (taskToUpdate) {
+            taskToUpdate.title = title;
+            taskToUpdate.description = description;
+            taskToUpdate.status = status;
+            response.writeHead(200, {"Content-Type": 'application/json'});
+            response.end(JSON.stringify(taskToUpdate));
+        
+        } else {
+
+        response.writeHead(404, {"Content-Type": 'application/json'});
+        response.end(JSON.stringify({message: 'Task not found'}))
+
+        }
+    
+    } else if (url.startsWith ('/tasks/') && method === 'DELETE') {
+
+        const index = tasks.findIndex (task => task.id === id )
+
+        if (index !== -1) {
+
+            tasks.splice(index, 1)
+            response.writeHead(204);
+            response.end();
+        } else {
+           
+        response.writeHead(404, {"Content-Type": 'application/json'});
+        response.end(JSON.stringify({message: 'Route not found'}))
+            
+        }
+        
+    } else  {
+        
+        response.writeHead(404, {"Content-Type": 'application/json'});
+        response.end(JSON.stringify({message: 'Route not found'}))
     }
+    });
 
 });
 
